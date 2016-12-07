9bcf03bf1458bb24f43eb9b3aaf59e2b4e7cf28c
zinman <guy.zinman@gmail.com>
2016-12-07 11:28:32 +0200

    Update and rename graph_functions3.scala to graph_functions4.scala

--- graph_functions4.scala
+++ graph_functions4.scala
@@ -4,10 +4,16 @@ import com.sparkbeyond.runtime.graph.MapWrapper
 import com.sparkbeyond.runtime.feature.types.USState
 
 
-object Zerocomplexity_graphFunctions3{
+object Zerocomplexity_graphFunctions4{
 	def zerocomplexity_adjacentNodes(graph: JGraphTWrapper[Int], vertex: Int) = 
 		graph.graph.outgoingEdgesOf(vertex).map(graph.graph.getEdgeTarget).toSeq
 
-	def zerocomplexity_lookupMany(seq: Seq[Int], lookup: MapWrapper[Int, USState]) = 
+	def zerocomplexity_lookupManyInt(seq: Seq[Int], lookup: MapWrapper[Int, Int]) = 
+		seq.flatMap(lookup.get)
+		
+	def zerocomplexity_lookupManyDouble(seq: Seq[Int], lookup: MapWrapper[Int, Double]) = 
+		seq.flatMap(lookup.get)
+	
+	def zerocomplexity_lookupManyString(seq: Seq[Int], lookup: MapWrapper[Int, String]) = 
 		seq.flatMap(lookup.get)
 }
