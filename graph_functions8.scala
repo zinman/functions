73480b9c454560723047e99b94639c12d7e51d5e
zinman <guy.zinman@gmail.com>
2016-12-07 15:20:57 +0200

    Update and rename graph_functions7.scala to graph_functions8.scala

--- graph_functions8.scala
+++ graph_functions8.scala
@@ -6,27 +6,27 @@ import com.sparkbeyond.runtime.feature.types.Identifier
 import scala.collection.GenTraversableOnce
 
 
-object Zerocomplexity_graphFunctions_7{
-	def zerocomplexity_outgoingNodes_7(graph: JGraphTWrapper[Identifier], vertex: Identifier) = 
+object Zerocomplexity_graphFunctions_8{
+	def zerocomplexity_outgoingNodes_8(graph: JGraphTWrapper[Identifier], vertex: Identifier) = 
 		graph.graph.outgoingEdgesOf(vertex).map(graph.graph.getEdgeTarget).toSeq
 
-	def zerocomplexity_incomingNodes_7(graph: JGraphTWrapper[Identifier], vertex: Identifier) = 
+	def zerocomplexity_incomingNodes_8(graph: JGraphTWrapper[Identifier], vertex: Identifier) = 
 		graph.graph.incomingEdgesOf(vertex).map(graph.graph.getEdgeSource).toSeq
 
 	
-	def zerocomplexity_lookupManyInt_7(seq: Seq[Identifier], lookup: MapWrapper[Identifier, Int]) = 
+	def zerocomplexity_lookupManyInt_8(seq: Seq[Identifier], lookup: MapWrapper[Identifier, Int]) = 
 		seq.flatMap(lookup.get)
 		
-	def zerocomplexity_lookupManyDouble_7(seq: Seq[Identifier], lookup: MapWrapper[Identifier, Double]) = 
+	def zerocomplexity_lookupManyDouble_8(seq: Seq[Identifier], lookup: MapWrapper[Identifier, Double]) = 
 		seq.flatMap(lookup.get)
 	
-	def zerocomplexity_lookupManyString_7(seq: Seq[Identifier], lookup: MapWrapper[Identifier, String]) = 
+	def zerocomplexity_lookupManyString_8(seq: Seq[Identifier], lookup: MapWrapper[Identifier, String]) = 
 		seq.flatMap(lookup.get)
 	
-	def zerocomplexity_lookupManyState_7(seq: Seq[Identifier], lookup: MapWrapper[Identifier, USState]) = 
+	def zerocomplexity_lookupManyState_8(seq: Seq[Identifier], lookup: MapWrapper[Identifier, USState]) = 
 		seq.flatMap(lookup.get)
 	
-	def zerocomplexity_averageInt_7(i: GenTraversableOnce[Int]) = if (i.isEmpty) 0.0
+	def zerocomplexity_averageInt_8(i: GenTraversableOnce[Int]) = if (i.isEmpty) 0.0
 	else {
 		var size = 0.0
 		val sum = (0l /: i)((sum, next) => {
@@ -36,7 +36,7 @@ object Zerocomplexity_graphFunctions_7{
 		sum / size
 	}
 	
-	def zerocomplexity_average_7(i: GenTraversableOnce[Double]) = if (i.isEmpty) 0.0
+	def zerocomplexity_average_8(i: GenTraversableOnce[Double]) = if (i.isEmpty) 0.0
 	else {
 		var size = 0
 		var sum = 0.0
